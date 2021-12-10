24c40ebd6a18d0c1fd74e27b3296a9e7977e29c4
nimou <741783559@qq.com>
2021-12-11 01:37:31 +0800

    push commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,6 +8,6 @@ public class GitTest {
         System.out.println("master test!");
         System.out.println("hello git2");
         System.out.println("hot-fix test!");
-        System.out.println("push test!");
+        System.out.println("pushtest!");
     }
 }
