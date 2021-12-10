358c7e8a6e37f9349692f1af87dcce8e6cbc018e
nimou <741783559@qq.com>
2021-12-11 01:01:14 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,6 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git1");
-        System.out.println("hello git3");
+        System.out.println("hello git2");
     }
 }
