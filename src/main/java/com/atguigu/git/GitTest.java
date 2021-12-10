8401f27377a8fe5cae3b53f492d1ff1a6b7ee9ff
zz7417 <65137988+zz7417@users.noreply.github.com>
2021-12-11 01:45:32 +0800

    pull commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,6 +8,7 @@ public class GitTest {
         System.out.println("master test!");
         System.out.println("hello git2");
         System.out.println("hot-fix test!");
-        System.out.println("pushtest!");
+        System.out.println("push test!");
+        System.out.println("pull test!");
     }
 }
